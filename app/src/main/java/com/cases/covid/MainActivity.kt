062ae2e88575ff138ff022f738b99a863c74657b
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2020-11-19 19:12:30 +0530

    "Basic UI created"

--- app/src/main/java/com/cases/covid/MainActivity.kt
+++ app/src/main/java/com/cases/covid/MainActivity.kt
@@ -2,10 +2,14 @@ package com.cases.covid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
