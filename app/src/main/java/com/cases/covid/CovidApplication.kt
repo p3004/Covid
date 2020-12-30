7691599a011ff1b041f005c2c3b71d725f38dcc1
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2020-12-30 18:19:12 +0530

    "ApplicationComponent is injected in CovidApplication.kt class to provide singleton dependencies application wide"

--- app/src/main/java/com/cases/covid/CovidApplication.kt
+++ app/src/main/java/com/cases/covid/CovidApplication.kt
@@ -1,14 +1,28 @@
 package com.cases.covid
 
 import android.app.Application
+import com.cases.covid.di.component.ApplicationComponent
+import com.cases.covid.di.component.DaggerApplicationComponent
+import com.cases.covid.di.module.ApplicationModule
 
 /**
  * Created by Pallab Banerjee on 11/20/2020.
  */
 class CovidApplication : Application() {
 
+    lateinit var applicationComponent: ApplicationComponent
+
     override fun onCreate() {
+        injectApplicationDependencies()
         super.onCreate()
+    }
+
+
+    private fun injectApplicationDependencies(){
+        applicationComponent = DaggerApplicationComponent.builder()
+            .applicationModule(ApplicationModule(this))
+            .build()
+        applicationComponent.inject(this)
     }
 
 }
