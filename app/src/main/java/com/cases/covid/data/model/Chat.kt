5d4c32149120f824becf2a71376a6fa6a0cc17bc
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-04-08 22:42:40 +0530

    "Chat data class created to hold data and type of view for ChatRecyclerView ChatAdapter class created to hold chat data and populate into the recyclerview"

---
+++ app/src/main/java/com/cases/covid/data/model/Chat.kt
@@ -0,0 +1,9 @@
+package com.cases.covid.data.model
+
+/**
+ * Created by Pallab Banerjee on 2/2/2021.
+ */
+data class Chat(
+val viewType : Int,
+val message : String
+)
---
+++ app/src/main/java/com/cases/covid/ui/main/ChatAdapter.kt
@@ -0,0 +1,82 @@
+package com.cases.covid.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.cases.covid.R
+import com.cases.covid.data.model.Chat
+import kotlinx.android.synthetic.main.incoming_chat_item.view.tvIncomingText
+import kotlinx.android.synthetic.main.outgoing_chat_item.view.*
+
+/**
+ * Created by Pallab Banerjee on 12/30/2020.
+ */
+
+class ChatAdapter : ListAdapter<Chat,RecyclerView.ViewHolder>(ChatDiffCallBack()) {
+
+    companion object{
+        private const val VIEW_TYPE_INCOMING_MESSAGE = 1;
+        private const val VIEW_TYPE_OUTGOING_MESSAGE = 2;
+    }
+
+
+    override fun getItemViewType(position: Int): Int {
+        return getItem(position).viewType
+    }
+
+
+
+
+    class ChatInViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val tvIncomingText: TextView = itemView.tvIncomingText
+
+        fun bind(chat: Chat) {
+              tvIncomingText.text = chat.message
+        }
+
+    }
+
+    class ChatOutViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val tvOutGoingChat: TextView = itemView.tvOutGoingChat
+
+        fun bind(chat: Chat) {
+            tvOutGoingChat.text = chat.message
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+         if(viewType == VIEW_TYPE_INCOMING_MESSAGE){
+             return  ChatInViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.incoming_chat_item,parent,false))
+        }
+        return ChatOutViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.incoming_chat_item,parent,false)
+        )
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(getItem(position).viewType == VIEW_TYPE_INCOMING_MESSAGE){
+            (holder as ChatInViewHolder).bind(getItem(position))
+        }
+        else{
+            (holder as ChatOutViewHolder).bind(getItem(position))
+        }
+    }
+
+
+}
+
+
+private class ChatDiffCallBack : DiffUtil.ItemCallback<Chat>(){
+    override fun areItemsTheSame(oldItem: Chat, newItem: Chat): Boolean  = oldItem.message == newItem.message
+
+    override fun areContentsTheSame(oldItem: Chat, newItem: Chat): Boolean = oldItem == newItem
+
+}
