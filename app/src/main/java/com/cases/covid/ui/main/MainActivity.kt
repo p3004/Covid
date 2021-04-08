0c236b44d92155eb4c59c87665c788af99d70c46
Pallab Banerjee <50319302+p3004@users.noreply.github.com>
2021-04-08 23:12:07 +0530

    "Dependencies are injected in MainActivity.kt and databinding is set to be true in build.gradle"

--- app/src/main/java/com/cases/covid/ui/main/MainActivity.kt
+++ app/src/main/java/com/cases/covid/ui/main/MainActivity.kt
@@ -2,14 +2,42 @@ package com.cases.covid.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cases.covid.CovidApplication
 import com.cases.covid.R
+import com.cases.covid.di.component.DaggerActivityComponent
+import com.cases.covid.di.module.ActivityModule
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var chatAdapter: ChatAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        injectDependencies()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+
+    private fun injectDependencies(){
+        DaggerActivityComponent.builder()
+            .applicationComponent((application as CovidApplication).applicationComponent)
+            .activityModule(ActivityModule(this))
+            .build()
+            .inject(this)
+
+    }
+
+
+    private fun setUpView(){
+
+
+
+
+    }
 
 }
